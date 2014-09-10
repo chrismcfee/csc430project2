254ca0d38645e8ce8be035038847d83bbab1982e
charlenex <unwanted.4vr@gmail.com>
2014-09-10 01:33:22 -0400

    Update payroll.h

--- payroll.h
+++ payroll.h
@@ -4,28 +4,76 @@
 using namespace std;
 
 class Employee {
-    private:
+  private:
+  //should have ID (for not douplicate data)
+  //string id;
     string name;
     string benefitDescription;
     double tax;
     double grossIncome;
     double netpay;
   public:
+    //void setid(string index);
+    //string getid();
     void setName(string);
     void getName();
     void getBenefits(string);
     void setBenefits();
-    void setTaxes(double);
-    void getTaxes();
+    void setTaxes(double value);
+    double getTaxes();
     void setGrossIncome(double);
     void getGrossIncome();
     void getNetPay();
     //...
     //function for alg change goes here
     //function for alg change goes here
-    //readin function
+    
+    
+    //readin function //done!
+    friend std::istream& operator >> (std::istream& ins, Employee& index)
+    {
+        //ins >> id;
+        ins >> name;
+        ins >> benefitDescription;
+        ins >> tax;
+        ins >> grossIncome;
+        ins >> netpay;
+        return ins;
+        //so now we can use it like an int or string (cin, fstream...)
+    }
+    
+    //comparation function (for extend process)
+    void operator = (Employee& source)
+    {
+        name = source.getName();
+        benefitDescription = source.getBenefits();
+        tax = source.getTaxes;
+        grossIncome = source.getGrossIncome();
+        netpay = source.getNetPay();
+    }
+    
+    //[IMPORTANT] those two functions cannot be written outsite
 };
+
+void Employee::setTaxes(double value)
+{
+    tax = value;   
+}
+
+double Employee::getTaxes()
+{
+    return tax;
+}
+
+/*TuanNguyen: Do you guys write the program in c++? so if it is then the all the functions should be like this:
+void setName(string index);
+the array should be dynamically so it can be extended if we have some kind of very large data file
+and if you guys also want the numberic checker I can also do that :">  */
+
 
 /*void Employee::setName (char newName[]) {
   name = newName;
   }*/
+  
+  //for this one: the array should be in an other header file with a separated class for ezier entering or modifying]
+  //I also posted 3 files in dynamic folder as an example for dynamic array and the seperated class
