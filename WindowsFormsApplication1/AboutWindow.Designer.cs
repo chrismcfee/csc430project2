873d3c45ac76c7ede7da58b043879ad8abe46400
Christopher <ggmethos@autistici.org>
2014-11-30 14:47:56 -0500

    slightly changed layout of about window
    just a tad

--- WindowsFormsApplication1/AboutWindow.Designer.cs
+++ WindowsFormsApplication1/AboutWindow.Designer.cs
@@ -47,7 +47,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
-            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Location = new System.Drawing.Point(21, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(127, 22);
             this.label1.TabIndex = 0;
